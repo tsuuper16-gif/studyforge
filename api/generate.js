c14053ed1741b09f748c5f45d4f8b819c637bd66
tsuuper16-gif <tsuuper16@gmail.com>
2026-06-10 22:18:29 +0100

    fix ESM module syntax

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,4 @@
-const https = require('https');
-
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
